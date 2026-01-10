68d667478a787a1aa70dd13e9be32f4377caa5ff
hasinivuppu26 <hasinivuppu1052@gmail.com>
2026-01-10 12:16:23 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -1,16 +1,37 @@
 function dispMessage(){
-    // a=10
-    // b=20
-    // c=a+b
-    //alert("Hello")
-    // alert(document.getElementById("txtEmail").value)
-   // lblMessage.innerHTML = document.getElementById("txtEmail").value
+    //     a=10
+    //     b=20
+    //     c=a+b
+    //     alert("Hello")
+    //     alert(document.getElementById("txtEmail").value)
+    //    lblMessage.innerHTML = document.getElementById("txtEmail").value
     // lblPassword.innerHTML= document.getElementById("txtPassword").value
     let email = document.getElementById("txtEmail").value
     let password = document.getElementById("txtPassword").value
     if(email==="hasinivuppu1052@gmail.com" && password==="1234"){
         lblMessage.innerHTML = "Welcome Hasini";
+        lblMessage.style.color = "green";
     }else{
         lblMessage.innerHTML = "Access Denied";
+        lblMessage.style.color = "red";
     }
+}
+function showLoginForm(){
+    let str = `
+      <h3>Login Form</h3>
+      <p><label id="lblMessage"></label></p>
+      <p><label id="lblPassword"></label></p>
+      <p><input type="text" id="txtEmail" placeholder="Email"></p>
+      <p><input type="password" id="txtPassword" placeholder="Password"></p>
+      <p><button class="login-btn" onclick="dispMessage()">Login</button></p>
+      <hr>
+      <p><button class="register-btn" onclick = "showRegisterForm()">Create Account</button></p>
+    `;
+    root.innerHTML = str;
+}
+function showRegisterForm(){
+    let str=`
+    <h1>Registration Form</h1>
+    `;
+    root.innerHTML = str;
 }
