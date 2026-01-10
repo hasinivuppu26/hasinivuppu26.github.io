8a4ceedcad2c336ea96e37670d7ccd2861e57c6b
hasinivuppu26 <hasinivuppu1052@gmail.com>
2026-01-10 11:52:13 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -4,6 +4,13 @@ function dispMessage(){
     // c=a+b
     //alert("Hello")
     // alert(document.getElementById("txtEmail").value)
-    lblMessage.innerHTML = document.getElementById("txtEmail").value
-    lblPassword.innerHTML = document.getElementById("txtPassword").value
+   // lblMessage.innerHTML = document.getElementById("txtEmail").value
+    // lblPassword.innerHTML= document.getElementById("txtPassword").value
+    let email = document.getElementById("txtEmail").value
+    let password = document.getElementById("txtPassword").value
+    if(email==="hasinivuppu1052@gmail.com" && password==="1234"){
+        lblMessage.innerHTML = "Welcome Hasini";
+    }else{
+        lblMessage.innerHTML = "Access Denied";
+    }
 }
