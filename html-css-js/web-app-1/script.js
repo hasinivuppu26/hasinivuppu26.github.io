149f6052a03e913329af8948beaf7e9332c18046
hasinivuppu26 <hasinivuppu1052@gmail.com>
2026-01-10 12:36:39 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -32,6 +32,24 @@ function showLoginForm(){
 function showRegisterForm(){
     let str=`
     <h1>Registration Form</h1>
+    <p><label id="lblRegister"></label></p>
+    <p><input type="text" id="txtEmail" placeholder="Email"></p>
+    <p><input type="password" id="txtPassword" placeholder="Password"></p>
+    <p><button class="register-btn" onclick="registerUser()">Register</button></p>
+    <hr>
+    <p><button class="register-btn" onclick = "showLoginForm()">Back to Login Form</button></p>
     `;
     root.innerHTML = str;
+}
+function registerUser(){
+    let email = document.getElementById("txtEmail").value
+    let password = document.getElementById("txtPassword").value
+    let msg = document.getElementById("lblRegister")
+    if (email === ""||password===""){
+        msg.innerHTML = "Please fill all the details";
+        msg.style.color = "red";
+    }else{
+        msg.innerHTML = "Successfully Registered";
+        msg.style.color = "green";
+    }
 }
