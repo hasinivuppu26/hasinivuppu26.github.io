721889a86f6d47cf359f1179fccd2f86871aca7f
hasinivuppu26 <hasinivuppu1052@gmail.com>
2026-01-10 14:31:40 +0530

    updated

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -2,19 +2,31 @@ function dispMessage(){
     //     a=10
     //     b=20
     //     c=a+b
-    //     alert("Hello")
+        // alert("Hello")
+//         const user = { 
+//          name: "Hasini",
+//          email:"hasinivuppu1052@gmail.com",
+//          password: "1234",
+// };
+//         console.log(user.name);
+//         console.log("Hello")
+    //  const arr = ["Maths","Science"];
+    //  console.log(arr[0]);
+    //  arr.push("English")
+    //  console.log(arr)
+
     //     alert(document.getElementById("txtEmail").value)
     //    lblMessage.innerHTML = document.getElementById("txtEmail").value
     // lblPassword.innerHTML= document.getElementById("txtPassword").value
-    let email = document.getElementById("txtEmail").value
-    let password = document.getElementById("txtPassword").value
-    if(email==="hasinivuppu1052@gmail.com" && password==="1234"){
-        lblMessage.innerHTML = "Welcome Hasini";
-        lblMessage.style.color = "green";
-    }else{
-        lblMessage.innerHTML = "Access Denied";
-        lblMessage.style.color = "red";
-    }
+    // let email = document.getElementById("txtEmail").value
+    // let password = document.getElementById("txtPassword").value
+    // if(email==="hasinivuppu1052@gmail.com" && password==="1234"){
+    //     lblMessage.innerHTML = "Welcome Hasini";
+    //     lblMessage.style.color = "green";
+    // }else{
+    //     lblMessage.innerHTML = "Access Denied";
+    //     lblMessage.style.color = "red";
+    // }
 }
 function showLoginForm(){
     let str = `
@@ -33,6 +45,7 @@ function showRegisterForm(){
     let str=`
     <h1>Registration Form</h1>
     <p><label id="lblRegister"></label></p>
+    <p><input type="text" id="txtName" placeholder="Name"></p>
     <p><input type="text" id="txtEmail" placeholder="Email"></p>
     <p><input type="password" id="txtPassword" placeholder="Password"></p>
     <p><button class="register-btn" onclick="registerUser()">Register</button></p>
@@ -41,11 +54,17 @@ function showRegisterForm(){
     `;
     root.innerHTML = str;
 }
+let users = [];
 function registerUser(){
+    let name = document.getElementById("txtName").value
     let email = document.getElementById("txtEmail").value
     let password = document.getElementById("txtPassword").value
     let msg = document.getElementById("lblRegister")
-    if (email === ""||password===""){
+    let user = {name:name,email:email,password:password}
+    users.push(user);
+    showLoginForm();
+    console.log(users);
+    if (name=== ""||email === ""||password===""){
         msg.innerHTML = "Please fill all the details";
         msg.style.color = "red";
     }else{
